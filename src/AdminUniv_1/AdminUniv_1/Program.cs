42e093578ea23bacfa9da6f2d9426b79afcecb6f
Esteban González Matamoros <esteb.gonza29@gmail.com>
2020-03-13 21:32:36 -0600

    Cambios para conexión a base
    Creación de clase específica para la BD en MySQL

--- src/AdminUniv_1/AdminUniv_1/Program.cs
+++ src/AdminUniv_1/AdminUniv_1/Program.cs
@@ -19,4 +19,67 @@ namespace AdminUniv_1
             Application.Run(new Form1());
         }
     }
+
+
+    public class DBManager
+    {
+        //Clase encargada de conectarse a la BD (MySQL) e interactuar con esta
+
+
+        private static String connString = "server = localhost; user id = root; database=tecvegetal";
+        
+        private static DBManager dbManager;
+
+        private DBManager()
+        {
+        }
+
+        public static DBManager getInstance()
+        {
+            if (dbManager == null)
+            {
+                dbManager = new DBManager();
+                connect();
+            }
+            return dbManager;
+        }
+
+        private static void connect()
+        {
+            try
+            {
+
+                String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+                String dbUrl = "localhost";
+                String user = "root";
+                String password = "";
+                try
+                {
+                    //Paso 2: Registra el driver
+                    Class.forName(JDBC_DRIVER);
+
+                    //Paso 3: Abre la conexión
+                    //System.out.println("Conectando a la Base de Datos...");
+                    DBManager.conn = DriverManager.getConnection(dbUrl, user, password);
+                    System.out.println("Conexión exitosa a la Base de Datos...");
+
+
+
+                }
+                catch (Exception e)
+                {
+                }
+
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+
+        
+
+
+
+    }
 }
