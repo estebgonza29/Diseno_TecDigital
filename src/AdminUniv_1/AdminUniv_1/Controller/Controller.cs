a96c77fd06eb0f0d766ebd1a183b8242d99fcdcc
Esteban González Matamoros <esteb.gonza29@gmail.com>
2020-03-15 13:42:15 -0600

    Creacion de las clases restantes del Controller

--- src/AdminUniv_1/AdminUniv_1/Controller/Controller.cs
+++ src/AdminUniv_1/AdminUniv_1/Controller/Controller.cs
@@ -7,134 +7,59 @@ using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System.Collections;
+using AdminUniv_1.Model;
+using AdminUniv_1.Controller.BDAccess;
 
 namespace AdminUniv_1.Controller
-{
-    public class DBAccess
-    {
-        //Clase encargada de conectarse a la BD (MySQL) e interactuar con esta
-
-
-        private static String connString = "server = localhost; user id = root; database=tecvegetal";
-
-        private static MySqlConnection conn;
-        private static MySqlCommand cmd;
-        private static DBAccess dbAccess;
-
-        private DBAccess()
-        {
-        }
-
-        public static DBAccess getInstance()
-        {
-            if (dbAccess == null)
-            {
-                dbAccess = new DBAccess();
-                connect();
-            }
-            return dbAccess;
-        }
-
-
-        public static MySqlConnection getCon()
-        {
-            return conn;
-        }
-
-        public MySqlCommand procedureDB(String proc)
-        {
-            try
-            {
-                MySqlCommand cmd = new MySqlCommand("proc", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                return cmd;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-
-        }
-
-        private static void connect()
-        {
-            try
-            {
-
-                //Abre la conexión
-                //System.out.println("Conectando a la Base de Datos...");
-                DBAccess.conn = new MySqlConnection("server = localhost; user id = root; database=tecvegetal; password=");
-                conn.Open();
-
-            }
-            catch (Exception e)
-            {
-            }
-        }
-
-
-        public DataSet queryDB(String query)
-        {
-            try
-            {
-                cmd = new MySqlCommand(query, conn);
-                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-                adp.Fill(ds);
-                return ds;
-
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
-
-        public void disconnectDB()
-        {
-            //finally block used to close resources
-            try
-            {
-                if (cmd != null)
-                {
-                    conn.Close();
-                }
-            }
-            catch (MySqlException se)
-            {
-            }// do nothing
-            try
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
-            catch (MySqlException se)
-            {
-                se.GetBaseException();
-            }//end finally try
-        }
-    }
+{    
 
     public class Controller
     {
-        private Controller controller;
+        private DTOGroupData dto { get; set; }
+        private groupManager mGroups { get; set; }
+        private courseManager mCourses { get; set; }
 
-        public Controller()
+        public Controller(DTOGroupData dto, groupManager mGroups, courseManager mCourses)
+        {
+            this.dto = dto;
+            this.mGroups = mGroups;
+            this.mCourses = mCourses;
+        }        
+
+        public void getGroup()
         {
 
         }
 
-        Controller getInstance()
+        public void loadCourses()
         {
-            if (controller == null)
-            {
-                controller = new Controller();
-            }
-            return controller;
+
         }
 
 
     }
+
+
+    public class DTOGroupData
+    {
+        private String courseID { get; set; }
+        private int groupNumber { get; set; }
+        private Group groupData { get; set; }
+        private LinkedList<Course> courseList { get; set; }
+
+        public DTOGroupData(String courseID, int groupNumber, Group groupData)
+        {
+            this.courseID = courseID;
+            this.groupNumber = groupNumber;
+            this.groupData = groupData;
+            courseList = new LinkedList<Course>();
+        }
+
+
+
+
+
+    }
+
+
 }
